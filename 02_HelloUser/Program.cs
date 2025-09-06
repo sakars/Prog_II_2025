e95fdb2b3c63edd4fdd42edf61ae792c3fcea2c2
Sakars <o.putaans@gmail.com>
2025-09-06 17:46:06 +0300

    Add project files.

---
+++ 02_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_HelloUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
